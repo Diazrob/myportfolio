91e8f7ee7009be9f52c21d904e90a8bdea9f9bbf
Diazrob <113378467+Diazrob@users.noreply.github.com>
2023-12-28 11:57:10 -0700

    with About

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,22 @@
 import React from 'react'
-import { HiArrowNarrowRight } from 'react-icons/hi'
+import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-white dark:bg-slate-900'>
-      {/* Container */}
-      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+    <div className='w-full h-screen text-gray-300 items-center justify-center'>
+      <div className='flex flex-col h-full justify-center max-w-[1000px] mx-auto px-6'>
         <p className='text-pink-600'>Hi, my name is</p>
-        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Robee Lou Diaz</h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer</h2>
-        <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I'm a full-stack developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I'm focused on building responsive full-stack web applications.
-        </p>
-        <div>
-          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-gray-600 hover:border-gray-600 justify-between'>
-            View Work 
-            <span className='group-hover:rotate-90 duration-300'>
-              <HiArrowNarrowRight className='ml-3' />
-            </span>
-            </button>
+        <h1 className='font-bold text-7xl'>
+          Robee Lou Diaz
+        </h1>
+        <h2 className='text-6xl text-gray-500 font-bold'>I'm a FullStack Developer</h2>
+        <p className='text-gray-500 py-4 max-w-[700px]'>I'm a fullstack developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I'm focused on building responsive full-stack web applications.</p>
+        <div >
+          <button className='flex group items-center border p-4 hover:bg-pink-600 hover:border-pink-600'>View Work <span className='pl-2 group-hover:rotate-90 duration-300 '><HiArrowNarrowRight />
+         </span></button>
         </div>
       </div>
+
     </div>
   )
 }
